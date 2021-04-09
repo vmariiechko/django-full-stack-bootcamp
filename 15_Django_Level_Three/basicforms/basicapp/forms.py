450b2fba9916fcf9f8b848bd372c2e37c2d01fae
marik348 <vadimich348@gmail.com>
2021-04-09 16:45:18 +0200

    Add clean method for entire form

--- 15_Django_Level_Three/basicforms/basicapp/forms.py
+++ 15_Django_Level_Three/basicforms/basicapp/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from django.core import validators
 
-def check_for_z(value):
-    if value[0].lower() != 'z':
-        raise forms.ValidationError("Name must start with Z/z")
-
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['verify_email']
+
+        if email != vemail:
+            raise forms.ValidationError("Email doesn't match!")
