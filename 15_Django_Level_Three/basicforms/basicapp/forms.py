212ab3cb89f1f8f7318616040b177e5a44be8232
marik348 <vadimich348@gmail.com>
2021-04-09 16:41:19 +0200

    Add mannual validation for single field

--- 15_Django_Level_Three/basicforms/basicapp/forms.py
+++ 15_Django_Level_Three/basicforms/basicapp/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.core import validators
 
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError("Name must start with Z/z")
+
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput,
-                                 validators=[validators.MaxLengthValidator(0)])
