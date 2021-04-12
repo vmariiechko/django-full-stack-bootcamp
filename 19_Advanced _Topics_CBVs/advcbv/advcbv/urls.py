91a12cd576776fb26b2d386bc7edf0496dc1462a
marik348 <vadimich348@gmail.com>
2021-04-12 21:21:30 +0200

    Add Advanced Topics - CBVs

--- 19_Advanced _Topics_CBVs/advcbv/advcbv/urls.py
+++ 19_Advanced _Topics_CBVs/advcbv/advcbv/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.CBView.as_view())
+    path('', views.IndexView.as_view()),
+    path('basic_app/', include('basic_app.urls', namespace="basic_app"))
 ]
--- 19_Advanced _Topics_CBVs/advcbv/basic_app/views.py
+++ 19_Advanced _Topics_CBVs/advcbv/basic_app/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views.generic import (View, TemplateView, ListView, DetailView,
+                                  CreateView, UpdateView, DeleteView)
+from . import models
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class Based Views are Cool!")
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
